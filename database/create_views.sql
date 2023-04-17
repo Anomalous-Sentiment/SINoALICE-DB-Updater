ff46e3b7f306da7d6eea11d1d7d74944c13e1d08
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-04-17 20:29:01 +0800

    Updated db views to work with incomplete GC data

--- database/create_views.sql
+++ database/create_views.sql
@@ -5,9 +5,14 @@ DROP VIEW IF EXISTS gc_matchups_id;
 CREATE OR REPLACE VIEW gc_matchups_id AS
     SELECT * from crosstab(
         $$
-            SELECT ARRAY[base.gvgeventid, base.guilddataid]::text[], base.gvgeventid, t.gvgtimetype, base.guilddataid, base.gcday, base.opponentguilddataid
+            SELECT ARRAY[base.gvgeventid, base.guilddataid]::text[], base.gvgeventid, transition.gvgtimetype, base.guilddataid, base.gcday, base.opponentguilddataid
             FROM gc_predictions base
-            INNER JOIN gc_data t USING (gvgeventid, guilddataid, gcday)
+            INNER JOIN
+            (
+              SELECT gd.guilddataid, gd.gvgeventid, gd.gcday, t.gvgtimetype
+              FROM gc_data gd
+              INNER JOIN timeslots t USING (gvgtimetype)
+            ) transition USING (guilddataid, gvgeventid)
             ORDER BY 1,2
         $$,
         $$
@@ -16,24 +21,29 @@ CREATE OR REPLACE VIEW gc_matchups_id AS
     ) as c(rn TEXT[], gvgeventid SMALLINT, gvgtimetype SMALLINT, guilddataid INTEGER, day_1 INTEGER, day_2 INTEGER, day_3 INTEGER, day_4 INTEGER, day_5 INTEGER, day_6 INTEGER);
 
     
+    
 
 DROP VIEW IF EXISTS gc_matchups;
 -- View to get table of guild matchups
 CREATE OR REPLACE VIEW gc_matchups AS
     SELECT * from crosstab(
         $$
-            SELECT ARRAY[base.gvgeventid, base.guilddataid]::text[], base.gvgeventid, ts.timeslot, g.guildname, base.gcday, og.guildname
+            SELECT ARRAY[base.gvgeventid, base.guilddataid]::text[], base.gvgeventid, base.guilddataid, transition.timeslot, g.guildname, base.gcday, og.guildname
             FROM gc_predictions base
-            INNER JOIN gc_data d USING (gvgeventid, guilddataid, gcday)
-            INNER JOIN guilds g ON g.guilddataid = base.guilddataid
-            INNER JOIN guilds og ON base.opponentguilddataid = og.guilddataid
-            INNER JOIN timeslots ts ON d.gvgtimetype = ts.gvgtimetype
+            INNER JOIN
+            (
+              SELECT gd.guilddataid, gd.gvgeventid, t.timeslot 
+              FROM gc_data gd
+              INNER JOIN timeslots t USING (gvgtimetype)
+            ) transition USING (guilddataid, gvgeventid)
+            LEFT JOIN guilds g ON g.guilddataid = base.guilddataid
+            LEFT JOIN guilds og ON base.opponentguilddataid = og.guilddataid
             ORDER BY 1,2
         $$,
         $$
             SELECT DISTINCT gcday FROM gc_days ORDER BY 1
         $$
-    ) as c(rn TEXT[], gc_num SMALLINT, timeslot SMALLINT, guild TEXT, day_1 TEXT, day_2 TEXT, day_3 TEXT, day_4 TEXT, day_5 TEXT, day_6 TEXT);
+    ) as c(rn TEXT[], gc_num SMALLINT, guild_id INTEGER, timeslot SMALLINT, guild TEXT, day_1 TEXT, day_2 TEXT, day_3 TEXT, day_4 TEXT, day_5 TEXT, day_6 TEXT);
 
 -- View to display players logged in since a specified date
 CREATE OR REPLACE VIEW login_activity AS
